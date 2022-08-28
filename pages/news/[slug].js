989b178828696b1b8a283854f5dd223761e8eed1
godzidcrypto <godzidcrypto@gmail.com>
2022-08-28 23:11:21 +0800

    added author, styled slug page, cross-chain, added ToC

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -3,24 +3,87 @@ import Container from "../../components/container";
 import { getAllCryptoNews, getSingleCryptoNews } from "../../lib/api";
 import CoverImage from "../../components/cover-image";
 import PostBody from "../../components/post-body";
+import DateComponent from "../../components/date";
+import Solana from "../../assets/images/solana.png";
+import Ethereum from "../../assets/images/ethereum.png";
+import ContentfulImage from "../../components/contentful-image";
+import Avatar from "../../components/avatar";
 
 function NewsItem({ cryptoNews }) {
-  console.log("HERE", cryptoNews);
-  const { sys, title, caption, chain, featuredImage, videoLink, writeUp } =
-    cryptoNews[0];
+  const {
+    sys,
+    title,
+    caption,
+    chain,
+    featuredImage,
+    videoLink,
+    writeUp,
+    author,
+  } = cryptoNews[0];
+
+  const { firstPublishedAt: date } = sys;
+
+  const content = writeUp.json.content;
+  const headings = content.filter((content) => {
+    return content.nodeType.includes("heading");
+  });
   return (
     <Layout>
-      <Container>
-        <div className="max-w-8xl mx-auto py-8">
-          <h1 className="text-8xl">{title}</h1>
-          <p className="text-2xl">{caption}</p>
-          <p>{chain}</p>
+      <div className="grid grid-cols-2 px-24 gap-8 pt-16">
+        <div>
+          <div className="mb-4 flex justify-between items-center">
+            {author && <Avatar name={author.name} picture={author.picture} />}
+            <p>
+              <DateComponent dateString={date} />
+            </p>
+          </div>
           <div>
             <CoverImage title={title} url={featuredImage.url} />
           </div>
+          {headings.length > 0 && (
+            <div className="mt-4 bg-[#F9F9F9] p-6 rounded-md">
+              <span className="text-xl">Table of Contents</span>
+              {headings.map((heading, index) => {
+                const text = heading.content[0].value;
+                const headingNumber = heading.nodeType.split("-")[1];
+                return (
+                  <li
+                    key={index}
+                    className="py-1 hover:underline"
+                    style={{
+                      paddingLeft: `${headingNumber - 2}rem`,
+                    }}
+                  >
+                    {/* {". ".repeat(headingNumber)} */}
+                    {text}
+                  </li>
+                );
+              })}
+            </div>
+          )}
         </div>
-        <PostBody content={writeUp} />
-      </Container>
+        <div>
+          <div className="flex items-center">
+            <h1 className="text-6xl">{title}</h1>
+            <div className="ml-6">
+              {chain?.length === 1 && chain[0] === "Solana" ? (
+                <ContentfulImage src={Solana} width={35} height={35} />
+              ) : chain?.length === 1 && chain[0] === "Ethereum" ? (
+                <ContentfulImage src={Ethereum} width={35} height={35} />
+              ) : chain?.length === 2 ? (
+                <>
+                  <ContentfulImage src={Solana} width={35} height={35} />
+                  <ContentfulImage src={Ethereum} width={35} height={35} />
+                </>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+          <p className="text-sm">{caption}</p>
+          <PostBody content={writeUp} />
+        </div>
+      </div>
     </Layout>
   );
 }
--- pages/news/index.js
+++ pages/news/index.js
@@ -4,16 +4,13 @@ import { getAllCryptoNews } from "../../lib/api";
 import CoverImage from "../../components/cover-image";
 import Link from "next/link";
 import News from "../../assets/images/news.svg";
-import Image from "next/image";
 import DateComponent from "../../components/date";
 import Solana from "../../assets/images/solana.png";
 import Ethereum from "../../assets/images/ethereum.png";
+import ContentfulImage from "../../components/contentful-image";
+import Avatar from "../../components/avatar";
 
 function NewsCollection({ allCryptoNews }) {
-  console.log(allCryptoNews);
-  const myLoader = ({ src }) => {
-    return `${src}`;
-  };
   return (
     <Layout>
       <div className="bg-[#8C50EE] text-black px-24 mx-auto flex items-center justify-between">
@@ -26,70 +23,68 @@ function NewsCollection({ allCryptoNews }) {
             cupidatat officia pariatur eiusmod quis fugiat.
           </p>
         </div>
-        <Image src={News} width={500} height={500} loader={myLoader} />
+        <ContentfulImage src={News} width={500} height={500} />
       </div>
       <div
-        className="grid grid-cols-3 gap-8 py-8 bg-[#faf5ff] px-24"
         style={{
           backgroundImage: "radial-gradient(#d1d5db 1.15px, #faf5ff 1.15px)",
           backgroundSize: "20px 20px",
         }}
       >
-        {allCryptoNews.map((news, index) => {
-          const {
-            title,
-            slug,
-            caption,
-            chain,
-            featuredImage,
-            videoLink,
-            writeUp,
-            sys,
-          } = news;
-          return (
-            <div
-              key={index}
-              className="shadow-md p-4 rounded-lg relative bg-white hover:bg-[#111827] hover:text-white transition-colors duration-300"
-            >
-              <CoverImage
-                title={title}
-                url={featuredImage.url}
-                slug={slug}
-                route={"news"}
-              />
-              <h2 className="my-2">
-                <Link href={`/news/${slug}`}>
-                  <a className="text-xl hover:underline hover:cursor-pointer">
-                    {title}
-                  </a>
-                </Link>
-              </h2>
-              <p className="text-sm my-2">{caption}</p>
-              <div className="flex justify-between mt-4">
-                <p>
-                  {chain === "Solana" ? (
-                    <Image
-                      src={Solana}
-                      width={35}
-                      height={35}
-                      loader={myLoader}
-                    />
-                  ) : (
-                    <Image
-                      src={Ethereum}
-                      width={35}
-                      height={35}
-                      loader={myLoader}
-                    />
+        {/* <div className="px-24 py-12 bg-red-600">Hello</div> */}
+        <div className="grid grid-cols-3 gap-8 py-8 px-24">
+          {allCryptoNews.map((news, index) => {
+            const { title, slug, caption, chain, featuredImage, sys, author } =
+              news;
+            return (
+              <div
+                key={index}
+                className="shadow-md p-4 rounded-lg relative bg-white hover:bg-[#111827] hover:text-white transition-colors duration-300"
+              >
+                <CoverImage
+                  title={title}
+                  url={featuredImage.url}
+                  slug={slug}
+                  route={"news"}
+                />
+                <h2 className="my-2">
+                  <Link href={`/news/${slug}`}>
+                    <a className="text-xl hover:underline hover:cursor-pointer">
+                      {title}
+                    </a>
+                  </Link>
+                </h2>
+                <p className="text-sm my-2">{caption}</p>
+                <div className="flex justify-between mt-4 items-center">
+                  {author && (
+                    <Avatar name={author.name} picture={author.picture} />
                   )}
-                </p>
-                <p>
-                  <DateComponent dateString={sys.firstPublishedAt} />
-                </p>
+                  <div>
+                    {chain?.length === 1 && chain[0] === "Solana" ? (
+                      <ContentfulImage src={Solana} width={35} height={35} />
+                    ) : chain?.length === 1 && chain[0] === "Ethereum" ? (
+                      <ContentfulImage src={Ethereum} width={35} height={35} />
+                    ) : chain?.length === 2 ? (
+                      <>
+                        <ContentfulImage src={Solana} width={35} height={35} />
+                        <ContentfulImage
+                          src={Ethereum}
+                          width={35}
+                          height={35}
+                        />
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <p>
+                    <DateComponent dateString={sys.firstPublishedAt} />
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </Layout>
   );
