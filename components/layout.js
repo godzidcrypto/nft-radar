822501a7303b238b6110d1661c01e0252cb35bc9
godzidcrypto <godzidcrypto@gmail.com>
2022-09-20 08:44:15 +0800

    added back the quotations

--- components/layout.js
+++ components/layout.js
@@ -19,7 +19,7 @@ export default function Layout({ hide = false, children, title }) {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, {
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
         page_path: window.location.pathname,
         });
     `}
