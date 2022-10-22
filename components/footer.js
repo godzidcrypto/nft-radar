b91fa072dfc2c8dfbdd944e0fe44ea79a4bb6349
godzidcrypto <godzidcrypto@gmail.com>
2022-10-23 02:57:55 +0800

    updated footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,8 @@
 import Container from "./container";
-import { EXAMPLE_PATH } from "../lib/constants";
+import ContentfulImage from "./contentful-image";
+import Logo from "../assets/images/logoBlack.png";
+import Twitter from "../components/twitter";
+import Discord from "../components/discord";
 
 export default function Footer() {
   return (
@@ -11,29 +14,70 @@ export default function Footer() {
       }}
     >
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <div className="mb-8">
-            <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left lg:mb-0 lg:pr-4 lg:w-1/2">
-              NFT Radar
-            </h3>
-            <p className="max-w-sm lg:max-w-lg text-center lg:text-left font-light">
-              For any site related questions or project feature/collaboration
-              requests, please DM HoTsAuCe#7292 on Discord
-            </p>
-          </div>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://twitter.com/solnftradar"
-              className="mx-3 bg-black hover:bg-[#8C50EE] rounded-md hover:text-black border border-black text-[#8C50EE] font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              View Twitter
-            </a>
-            <a
-              href={`https://discord.gg/nftradar`}
-              className="mx-3 font-bold hover:underline"
-            >
-              Join Discord
-            </a>
+        <div>
+          <div class="mx-auto max-w-screen-xl py-12">
+            <div class="mx-auto space-y-6">
+              <div className="mb-8">
+                <div className="flex justify-center gap-4 items-center mb-4">
+                  <ContentfulImage src={Logo} width={75} height={75} />
+                  <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:mb-0 lg:pr-4">
+                    NFT Radar
+                  </h3>
+                </div>
+                <p className="max-w-lg mx-auto text-center font-light">
+                  For any site-related questions or project
+                  feature/collaboration requests, please fill out this{" "}
+                  <a
+                    href="https://forms.gle/iCYmZPkMrcF5tNNVA"
+                    target="_blank"
+                    className="font-semibold hover:cursor-pointer hover:underline"
+                  >
+                    FORM
+                  </a>
+                </p>
+              </div>
+              <div class="flex justify-center gap-4 items-center">
+                <a
+                  class="text-gray-900 hover:text-gray-900/75"
+                  href="https://twitter.com/solnftradar"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Twitter fill={`"#ffffff"`} width={24} />
+                </a>
+
+                <a
+                  class="text-gray-900 hover:text-gray-900/75"
+                  href="https://discord.gg/nftradar"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="GitHub"
+                >
+                  <Discord fill={`"#ffffff"`} width={28} />
+                </a>
+
+                <a
+                  class="text-pink-600 hover:text-pink-600/75"
+                  href="https://magiceden.io/marketplace/radar_passes"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <svg
+                    width="40"
+                    height="22"
+                    viewBox="0 0 40 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="-ml-[1.5px]"
+                  >
+                    <path
+                      d="M28.3414 6.11623L30.6842 8.8689C30.9524 9.17808 31.1902 9.4324 31.2863 9.57701C31.987 10.2733 32.3798 11.2144 32.3793 12.195C32.3135 13.352 31.5596 14.1399 30.8613 14.9826L29.2218 16.9075L28.3667 17.9048C28.336 17.9392 28.3162 17.9817 28.3098 18.0271C28.3033 18.0724 28.3105 18.1187 28.3304 18.16C28.3503 18.2014 28.3821 18.2361 28.4218 18.2599C28.4615 18.2837 28.5074 18.2955 28.5539 18.2938H37.1003C38.4058 18.2938 40.0503 19.3909 39.9542 21.0564C39.9515 21.8135 39.6452 22.5387 39.102 23.074C38.5589 23.6093 37.8229 23.9112 37.0548 23.9138H23.6709C22.7905 23.9138 20.4224 24.0086 19.7595 21.989C19.6185 21.5669 19.5993 21.1146 19.7038 20.6824C19.8965 20.0434 20.2014 19.4425 20.6045 18.9072C21.2775 17.9098 22.0062 16.9125 22.7247 15.9451C23.6507 14.6784 24.602 13.4517 25.5381 12.1601C25.5713 12.1181 25.5894 12.0663 25.5894 12.013C25.5894 11.9597 25.5713 11.908 25.5381 11.8659L22.1377 7.87654C22.1156 7.84762 22.0869 7.82416 22.054 7.80801C22.021 7.79186 21.9848 7.78345 21.948 7.78345C21.9112 7.78345 21.8749 7.79186 21.842 7.80801C21.8091 7.82416 21.7804 7.84762 21.7582 7.87654C20.8474 9.08832 16.8601 14.454 16.01 15.5411C15.1599 16.6282 13.0651 16.6881 11.9063 15.5411L6.58819 10.2801C6.55422 10.2465 6.51089 10.2237 6.4637 10.2144C6.4165 10.2051 6.36758 10.2098 6.32312 10.228C6.27867 10.2462 6.24069 10.2769 6.21401 10.3164C6.18733 10.3559 6.17315 10.4023 6.17327 10.4497V20.5677C6.18578 21.2858 5.97004 21.9896 5.55626 22.5807C5.14248 23.1718 4.55141 23.6205 3.86588 23.864C3.42784 24.0142 2.9597 24.0591 2.5005 23.9948C2.04129 23.9306 1.60433 23.759 1.22604 23.4946C0.847758 23.2301 0.53911 22.8803 0.325838 22.4744C0.112565 22.0685 0.000847208 21.6183 0 21.1612V2.96961C0.0304872 2.31403 0.269847 1.6846 0.684067 1.17075C1.09829 0.656894 1.6663 0.284762 2.30738 0.107231C2.8573 -0.0371504 3.43617 -0.0357056 3.98533 0.111418C4.5345 0.258542 5.03444 0.546117 5.4345 0.945001L13.6115 9.01352C13.636 9.03802 13.6657 9.05693 13.6984 9.06888C13.7311 9.08084 13.7661 9.08556 13.8008 9.08271C13.8356 9.07985 13.8693 9.06949 13.8995 9.05236C13.9298 9.03523 13.9559 9.01176 13.9759 8.98359L19.7848 1.0597C20.0533 0.738016 20.3898 0.478058 20.7709 0.298026C21.152 0.117993 21.5684 0.0222402 21.991 0.0174712H37.1003C37.5138 0.018143 37.9224 0.105817 38.2987 0.274632C38.6751 0.443447 39.0105 0.689511 39.2826 0.996365C39.5547 1.30322 39.7572 1.66379 39.8765 2.05397C39.9958 2.44414 40.0292 2.85492 39.9744 3.25884C39.868 3.95952 39.5072 4.59836 38.9592 5.05687C38.4111 5.51537 37.7129 5.76243 36.9941 5.7522H28.5336C28.4911 5.75322 28.4497 5.76545 28.4136 5.78763C28.3775 5.80981 28.3481 5.84111 28.3285 5.87829C28.3088 5.91546 28.2997 5.95714 28.3019 5.99899C28.3042 6.04084 28.3178 6.08133 28.3414 6.11623Z"
+                      fill="black"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
