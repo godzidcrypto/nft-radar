0899cf415ddd1c19a689e7b0df0836b6912ad02c
godzidcrypto <godzidcrypto@gmail.com>
2022-10-29 03:00:04 +0800

    identified aptos chain for projects and news

--- components/entry-content.js
+++ components/entry-content.js
@@ -3,6 +3,7 @@ import ContentfulImage from "./contentful-image";
 import PostBody from "./post-body";
 import Solana from "../assets/images/solana.png";
 import Ethereum from "../assets/images/ethereum.png";
+import Aptos from "../assets/images/aptos.png";
 import DateComponent from "./date";
 import TableOfContents from "./table-of-contents";
 
@@ -20,18 +21,28 @@ function EntryContent({
     <div className="bg-[#16181C] py-6 px-6 lg:px-12 rounded-xl">
       <div className="mt-4 flex justify-between md:items-center flex-col md:flex-row">
         {author && <Avatar name={author.name} picture={author.picture} />}
-        <div className="lg:ml-8 my-4 lg:my-0">
-          {chain?.length === 1 && chain[0] === "Solana" ? (
-            <ContentfulImage src={Solana} width={35} height={35} />
-          ) : chain?.length === 1 && chain[0] === "Ethereum" ? (
-            <ContentfulImage src={Ethereum} width={35} height={35} />
-          ) : chain?.length === 2 ? (
-            <>
-              <ContentfulImage src={Solana} width={35} height={35} />
-              <ContentfulImage src={Ethereum} width={35} height={35} />
-            </>
-          ) : (
-            ""
+        <div>
+          {chain?.length > 0 && (
+            <div>
+              {chain.map((chain, index) => {
+                const image =
+                  chain === "Solana"
+                    ? Solana
+                    : chain === "Ethereum"
+                    ? Ethereum
+                    : Aptos;
+                if (chain !== false) {
+                  return (
+                    <ContentfulImage
+                      key={index}
+                      src={image}
+                      width={25}
+                      height={25}
+                    />
+                  );
+                }
+              })}
+            </div>
           )}
         </div>
         <p>
