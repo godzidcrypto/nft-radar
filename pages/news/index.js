37130c5e4815b114d76ab2248ccd79ca11dea4bf
godzidcrypto <godzidcrypto@gmail.com>
2022-10-29 02:21:28 +0800

    added hidden categories inside news as sub nav

--- pages/news/index.js
+++ pages/news/index.js
@@ -4,6 +4,7 @@ import News from "../../assets/images/news.svg";
 import Hero from "../../components/hero";
 import EntriesList from "../../components/entries-list";
 import Container from "../../components/container";
+import Link from "next/link";
 
 function NewsCollection({ allCryptoNews }) {
   return (
@@ -16,6 +17,23 @@ function NewsCollection({ allCryptoNews }) {
           }
           svg={News}
         />
+        <div className="grid grid-cols-3 text-center gap-2 sm:gap-4">
+          <Link href={"/opinions"}>
+            <a className="bg-gray-800 rounded-xl text-sm sm:text-base py-2 sm:p-4 sm:font-bold hover:underline opacity-80 hover:opacity-100">
+              Opinions
+            </a>
+          </Link>
+          <Link href={"/analysis"}>
+            <a className="bg-gray-800 rounded-xl text-sm sm:text-base py-2 sm:p-4 sm:font-bold hover:underline opacity-80 hover:opacity-100">
+              Analysis
+            </a>
+          </Link>
+          <Link href={"/interviews"}>
+            <a className="bg-gray-800 rounded-xl text-sm sm:text-base py-2 sm:p-4 sm:font-bold hover:underline opacity-80 hover:opacity-100">
+              Interviews
+            </a>
+          </Link>
+        </div>
         <div>
           {/* <div className="px-24 py-12 bg-red-600">Hello</div> */}
           <EntriesList
--- pages/opinions/index.js
+++ pages/opinions/index.js
@@ -4,6 +4,7 @@ import Opinions from "../../assets/images/opinions.svg";
 import Hero from "../../components/hero";
 import EntriesList from "../../components/entries-list";
 import Container from "../../components/container";
+import Link from "next/link";
 
 function OpinionsCollection({ allOpinionPieces }) {
   return (
@@ -15,6 +16,21 @@ function OpinionsCollection({ allOpinionPieces }) {
           svg={Opinions}
           reverse={true}
         />
+        <p className="py-6 underline font-bold flex gap-2">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.02698 11.9929L5.26242 16.2426L6.67902 14.8308L4.85766 13.0033L22.9731 13.0012L22.9728 11.0012L4.85309 11.0033L6.6886 9.17398L5.27677 7.75739L1.02698 11.9929Z"
+              fill="currentColor"
+            />
+          </svg>
+          <Link href={"/news"}>Back to News</Link>
+        </p>
         <div>
           {/* <div className="px-24 py-12 bg-red-600">Hello</div> */}
           <EntriesList
