7aa9d56e3d9a8d97bee74453c4809f1b5ee1323d
godzidcrypto <godzidcrypto@gmail.com>
2022-11-05 13:56:20 +0800

    added scavenger hunt page

---
+++ pages/oceandwarfsoosh7292.js
@@ -0,0 +1,9 @@
+function Oceandwarfsoosh7292() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p>4JZ449UAWs39zVsSHzCJqfkjGBQdD8hzZBE2fab5qJgr</p>
+    </div>
+  );
+}
+
+export default Oceandwarfsoosh7292;
